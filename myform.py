954740e86b024921ea4bd5abf5cee7e5283028ec
Toma <asistnik@gmail.com>
2022-03-24 09:19:47 +0300

    Исправление ошибки в регулярном выражении для email

--- myform.py
+++ myform.py
@@ -13,7 +13,7 @@ def my_form():
        return errormess
 import re
 
-regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})$')
 def howEmail(mail):
     if re.fullmatch(regex, mail):
       return "Thanks! The answer will be sent to the mail %s" % mail
