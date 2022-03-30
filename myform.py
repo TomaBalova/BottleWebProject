e634bf8c492a6a782faef8a8b6c11d78a895bead
Toma <asistnik@gmail.com>
2022-03-30 23:56:55 +0300

    Добавления заполнения файла JSON

--- myform.py
+++ myform.py
@@ -1,21 +1,36 @@
-from bottle import post, request 
+from bottle import post, request, template
 @post('/home', method='post') 
 
 
+
 def my_form(): 
-    mail = request.forms.get('ADRESS') 
+    import pdb
+    import json
+    #Запись в переменные введенные на форме данные
+    questions = {}
+    email = request.forms.get('ADRESS') 
     mess = request.forms.get('QUEST')
-    
-    if ((mail and mail != '') and (mess and mess != '')): return howEmail(mail)
+    questions[email] = mess
+    #Проверка на зполнение 
+    if ((email and email != '') and (questions[email] and questions[email] != '')): 
+        with open('dataJSON.txt', 'a') as outfile:
+            json.dump(questions, outfile)
+        return howEmail(email, mess)
     else:
        errormess = "Zapolnite vse polya pered otpravkoi formi"
-
+       #pdb.set_trace()
        return errormess
+    
 import re
 
-regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})$')
-def howEmail(mail):
+regex = re.compile(r'([a-zA-Z0-9]+[.-_])*[a-zA-Z0-9]+@[a-zA-Z0-9-]+(\.[A-Z|a-z]{2,})+') #Регулярное выражение для емаил
+def howEmail(mail, mess):
+    import pdb
+    qestions = {}
+    qestions[mail] = mess
     if re.fullmatch(regex, mail):
-      return "Thanks! The answer will be sent to the mail %s" % mail
+        #pdb.set_trace()
+        return "Thanks! The answer will be sent to the mail %s" % mail
     else:
-      return "Invalid email"
+        #pdb.set_trace()
+        return "Invalid email"
