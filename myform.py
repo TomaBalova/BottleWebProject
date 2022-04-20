18342631e5f152242c519ed6a0924775344d3418
Toma <asistnik@gmail.com>
2022-04-21 00:41:42 +0300

    Проект до интеграции unit-тестов

--- myform.py
+++ myform.py
@@ -6,18 +6,25 @@ from bottle import post, request, template
 def my_form(): 
     import pdb
     import json
+    from collections import defaultdict
+    #f = open('dataJSON.txt', 'w')
     #Запись в переменные введенные на форме данные
     questions = {}
+    with open('dataJSON.txt', 'r') as fl:
+        questions = json.load(fl)
     email = request.forms.get('ADRESS') 
     mess = request.forms.get('QUEST')
+    #questions.setdefault(email, [])
+    if email in questions:
+        mess += '|' + questions[email]
     questions[email] = mess
     #Проверка на зполнение 
     if ((email and email != '') and (questions[email] and questions[email] != '')): 
-        with open('dataJSON.txt', 'a') as outfile:
+        with open('dataJSON.txt', 'w') as outfile:
             json.dump(questions, outfile)
         return howEmail(email, mess)
     else:
-       errormess = "Zapolnite vse polya pered otpravkoi formi"
+       errormess = "Text-form is null"
        #pdb.set_trace()
        return errormess
     
